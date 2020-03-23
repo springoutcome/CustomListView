1d469b4ffad170707e59d66a41e0bd5aca421259
Yuuto Harada <b1018185.fun@gmail.com>
2020-03-23 15:30:25 +0900

    first commit

---
+++ CustomListView/AnotherActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CustomListView.Resources.layout
+{
+    [Activity(Label = "AnotherActivity")]
+    public class AnotherActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
